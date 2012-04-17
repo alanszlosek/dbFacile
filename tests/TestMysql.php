1972e051428f452f33cbd5fe10543f59710af158
Alan W Szlosek Jr <alan.szlosek@gmail.com>
2012-04-16 20:51:56 -0700

    closer

--- tests/TestMysql.php
+++ tests/TestMysql.php
@@ -1,13 +1,26 @@
 <?php
-include('TestSqlite3.php');
+require_once('../splitup/dbFacile_mysql.php');
+require('TestSqlite3.php');
 
 class TestMysql extends TestSqlite3 {
 
-        public static function setUpBeforeClass() {
+	public static function setUpBeforeClass() {
                 $db = new dbFacile_mysql();
 		$db->open('dbFacile', 'dbfacile', 'dbfacile');
-		$db->execute('create table test (b int(11) auto_increment, c text, primary key(b))');
-		Main::$db = $db;
+		$db->execute('drop table if exists test');
+		$db->execute('create table test (b int(11) primary key auto_increment, c text)');
 	}
+
+        protected function setUp() {
+                $db = new dbFacile_mysql();
+		$db->open('dbFacile', 'dbfacile', 'dbfacile');
+		$this->db = $db;
+	}
+	protected function tearDown() {
+		$this->db->close();
+	}
+	public static function tearDownAfterClass() {
+	}
+
 }
 
